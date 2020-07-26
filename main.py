b98314a2435eee0ca28b9015a2984021af18f8bb
inity8 <di848588@gmail.com>
2020-07-26 04:45:42 +0300

    add send action

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 # /main.py file
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# dependencies
+
 import telebot
 import os
 from telebot import types
 
 token = os.getenv('API_BOT_TOKEN')
 bot = telebot.TeleBot(token)
+
+known_users = []
+user_steps = {}
+commands = {
+    'start': 'Start using this bot',
+    'help': 'Useful information about this bot',
+    'contacts': 'Contacts'
+}
+
+
+def send_action(action):
+    """Send 'action' while processing  function command."""
+    def decorator(func):
+        @wraps(func)
+        def command_func(message, *args, **kwargs):
+            bot.send_chat_action(chat_id=message.chat.id, action=action)
+            return func(message, *args, **kwargs)
+
+        return command_func
+        return decorator
 
 
 @bot.message_handler(commands=['start'])
